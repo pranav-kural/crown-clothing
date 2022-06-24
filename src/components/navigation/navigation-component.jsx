4e5a72da82cf8760a8dcdcd979cc71c764b771fd
pranav-kural <pranavkural042@gmail.com>
2022-06-24 14:46:10 -0600

    added styled components to navigation

--- src/components/navigation/navigation-component.jsx
+++ src/components/navigation/navigation-component.jsx
@@ -1,41 +1,38 @@
 import { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import './styles/navigation-styles.scss';
 import { UserContext } from '../../contexts/user-context';
 import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
 import CartIcon from '../cart-icon/cart-icon-component';
 import CartDropdown from '../cart-dropdown/cart-dropdown-component';
 import { CartContext } from '../../contexts/cart-context';
 
+import {
+  LogoContainer,
+  NavigationContainer,
+  NavLink,
+  NavLinksContainer,
+} from './styles/navigation-styles';
+
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
   const { isCartOpen } = useContext(CartContext);
 
   return (
-    <nav className="navigation">
-      <Link className="logo-container" to="/">
+    <NavigationContainer>
+      <LogoContainer to="/">
         <CrownLogo className="logo" />
-      </Link>
-      <div className="nav-links-container">
-        <Link className="nav-link" to="/">
-          Home
-        </Link>
-        <Link className="nav-link" to="/shop">
-          Shop
-        </Link>
+      </LogoContainer>
+      <NavLinksContainer>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/shop">Shop</NavLink>
         {!currentUser ? (
-          <Link className="nav-link" to="/login">
-            Login
-          </Link>
+          <NavLink to="/login">Login</NavLink>
         ) : (
-          <Link className="nav-link" to="/logout">
-            Logout
-          </Link>
+          <NavLink to="/logout">Logout</NavLink>
         )}
         <CartIcon />
-      </div>
+      </NavLinksContainer>
       {isCartOpen && <CartDropdown />}
-    </nav>
+    </NavigationContainer>
   );
 };
 
