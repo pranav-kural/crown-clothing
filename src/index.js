41d59d8d1475ec2524860c3a140c58de55a6232c
pranav-kural <pranavkural042@gmail.com>
2022-06-26 18:16:51 -0600

    replaced cart context with redux store's cart reducer

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { CartProvider } from './contexts/cart-context';
 import { store } from './store/store';
 import App from './AppRoutes';
 import reportWebVitals from './reportWebVitals';
@@ -14,9 +13,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <CartProvider>
-          <App />
-        </CartProvider>
+        <App />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
