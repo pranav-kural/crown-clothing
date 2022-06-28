cbe0be36f37c272f04c98bc38e53d9ca2b734a75
pranav-kural <pranavkural042@gmail.com>
2022-06-28 16:41:33 -0600

    added code to use Redux DevTools if available

--- src/store/store.js
+++ src/store/store.js
@@ -39,9 +39,15 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = process.env.NODE_ENV === 'development' ? [logger] : [];
+const middleWares = process.env.NODE_ENV !== 'production' ? [logger] : [];
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composeEnhancer =
+  (process.env.NODE_ENV !== 'production' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 // create store
 export const store = createStore(
