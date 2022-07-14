38d7e7e39b402f4b8c4e398f192251357951d3f1
pranav-kural <pranavkural042@gmail.com>
2022-07-13 23:31:33 -0600

    added payment handler

--- src/components/checkout/payment-form/payment-form-component.jsx
+++ src/components/checkout/payment-form/payment-form-component.jsx
@@ -1,11 +1,24 @@
-import { CardElement } from '@stripe/react-stripe-js';
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import Button, { BUTTON_TYPES } from '../../button/button-component';
 import { FormContainer, PaymentFormContainer } from './payment-form-styles';
 
 const PaymentForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const paymentHandler = async (e) => {
+    e.preventDefault();
+
+    if (!stripe || !elements) {
+      //handler error
+      console.error('stripe payment failed');
+      return;
+    }
+  };
+
   return (
     <PaymentFormContainer>
-      <FormContainer>
+      <FormContainer onSubmit={paymentHandler}>
         <h2>Credit Card Payment: </h2>
         <CardElement />
         <Button buttonType={BUTTON_TYPES.google}>Pay now</Button>
